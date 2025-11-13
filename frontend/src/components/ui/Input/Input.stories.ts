5dc8a62ee0c3566bf6474e6a66e04a1e9a4e3f38
KimChangmin <zzangmin3@gmail.com>
2025-11-13 10:38:50 +0900

    feat: Input, TextArea border prop 추가 및 디자인 수정

--- frontend/src/components/ui/Input/Input.stories.ts
+++ frontend/src/components/ui/Input/Input.stories.ts
@@ -1,12 +1,21 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Input } from "./Input";
 
-const meta: Meta<typeof Input> = {
+const meta = {
   title: "UI/Input",
   component: Input,
-  tags: ["autodocs"]
-};
+  tags: ["autodocs"],
+  argTypes: {
+    placeholder: { control: "text" },
+    hasBorder: { control: "boolean" }
+  }
+} satisfies Meta<typeof Input>;
 
 export default meta;
 
-export const DefaultFooter: StoryObj<typeof Input> = {};
+export const DefaultInput: StoryObj<typeof Input> = {
+  args: {
+    placeholder: "값을 입력하세요",
+    hasBorder: true
+  }
+};
--- frontend/src/components/ui/TextArea/TextArea.tsx
+++ frontend/src/components/ui/TextArea/TextArea.tsx
@@ -1,26 +1,28 @@
-interface InputProps {
-  placeholder: string;
+interface TextAreaProps {
+  placeholder?: string;
+  rows?: number; // textarea가 표시할 고정 줄 높이
   hasBorder?: boolean; // border 유무 선택
 }
 
-export const Input = ({
+export const TextArea = ({
   placeholder,
+  rows = 4,
   hasBorder = true,
   ...props
-}: InputProps) => {
+}: TextAreaProps) => {
   return (
     <div
-      className={`flex items-center justify-start gap-2.5 overflow-hidden ${
+      className={`flex justify-start gap-2.5 overflow-hidden ${
         // hasBorder 값에 따른 조건부 스타일 적용
         hasBorder
           ? "rounded-lg border border-gray-300"
           : "rounded-lg border border-transparent"
       } px-4 py-1.5`}
     >
-      <input
-        type={"text"}
+      <textarea
         placeholder={placeholder}
-        className="flex-1 bg-transparent text-sm font-normal text-black outline-none"
+        rows={rows}
+        className="flex-1 resize-none bg-transparent text-sm font-normal text-black outline-none"
         {...props}
       />
     </div>
